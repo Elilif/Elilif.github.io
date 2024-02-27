a7d8aefad9e527c5c34bf102a5342f41e7b36c82
eli <qq1358722950@gmail.com>
2024-02-27 22:01:48 +0800

    Rebuilding site 2024-02-27 22:01:48

--- scripts/script.js
+++ scripts/script.js
@@ -1,113 +1,46 @@
-// based on https://www.roboleary.net/2022/01/13/copy-code-to-clipboard-blog.html
-const copyIcon = "<i class='bx bx-copy-alt' ></i>";
-const hideIcon = "<i class='bx bx-collapse-vertical' ></i>";
-const showIcon = "<i class='bx bx-expand-vertical' ></i>";
-const expandIcon = "<i class='bx bx-expand-horizontal'></i>";
-const originalIcon = "<i class='bx bx-reset' ></i>";
-const copyTitle = "Copy code";
-const hideTitle = "Hide code";
-const showTitle = "Show code";
-const expandTitle = "Expand code";
-const originalTitle = "Show original code";
-
-async function copyCode(block, button) {
-  let code = block.querySelector('.org-src-container:not([hidden])').querySelector('pre.src');
-  let text = code.innerText;
-  await navigator.clipboard.writeText(text);
-  button.innerText = 'Copied';
-  setTimeout(() => {
-    button.innerHTML = copyIcon;
-  }, 500);
-}
-
-function addButtons() {
+function hideExpandedCode() {
   let parents = document.querySelectorAll('.multilang');
   parents.forEach((parent) => {
-    let buttons = document.createElement('div');
-    buttons.classList.add('src-buttons');
-    parent.prepend(buttons);
-
-    // copy button
-    let copyButton = document.createElement('button');
-    copyButton.innerHTML = copyIcon;
-    copyButton.setAttribute('title', copyTitle);
-    copyButton.classList.add('copy-code');
-    buttons.prepend(copyButton);
-    copyButton.addEventListener('click', async() => {
-      await copyCode(parent, copyButton);
-    });
-
-    // switch button
     let srcBlocks = parent.querySelectorAll('.org-src-container');
     let orig = srcBlocks[0];
     let expanded = srcBlocks[1];
-    let switchButton = document.createElement('button');
-    let origPre = orig.querySelector('.src');
-    let expandedPre = expanded.querySelector('.src');
     let label = orig.querySelector('.org-src-name');
 
     if (label) {
       expanded.prepend(label.cloneNode(true));
     }
 
-    switchButton.innerHTML = expandIcon;
-    switchButton.setAttribute('title', expandTitle);
-    switchButton.classList.add('code-switcher');
-    buttons.prepend(switchButton);
-
     expanded.hidden = true;
+  });
+}
 
-    switchButton.addEventListener('click', function() {
-      if (expanded.hidden) {
-        let id = origPre.id;
-        expandedPre.id = id;
-        origPre.removeAttribute('id');
-        expanded.removeAttribute('hidden');
-        switchButton.innerHTML = originalIcon;
-        switchButton.setAttribute('title', originalTitle);
-        orig.hidden = true;
-      } else {
-        let id = expandedPre.id;
-        origPre.id = id;
-        expandedPre.removeAttribute('id');
-        orig.removeAttribute('hidden');
-        switchButton.innerHTML = expandIcon;
-        switchButton.setAttribute('title', expandTitle);
-        expanded.hidden = true;
-      }
+function addExpandButton() {
+  let parents = document.querySelectorAll('.multilang');
+  parents.forEach((parent) => {
+    let expandSection = document.createElement('div');
+    const selectedClass = 'selected';
+
+    expandSection.classList.add('src-expand-section');
+    ['noweb', 'expand'].forEach((elt) => {
+      let button = document.createElement('span');
+      button.innerText = elt;
+      button.classList.add('src-' + elt);
+      expandSection.append(button);
     });
 
-    // hide button
-    let hideButton = document.createElement('button');
-    hideButton.innerHTML = hideIcon;
-    hideButton.setAttribute('title', hideTitle);
-    hideButton.classList.add('hide-code');
-    buttons.prepend(hideButton);
-
-    // let pres = parent.querySelectorAll('.src');
-    //   pres.forEach((pre) => {
-    //     pre.hidden = true;
-    //   });
-    // switchButton.hidden = true;
-
-    hideButton.addEventListener('click', () => {
-      let pres = parent.querySelectorAll('.src');
-      if (parent.querySelectorAll('.src:not([hidden])').length != 0) {
-        pres.forEach((pre) => {
-          pre.hidden = true;
-        });
-        switchButton.hidden = true;
-        hideButton.innerHTML = showIcon;
-        hideButton.setAttribute('title', showTitle);
-      } else {
-        pres.forEach((pre) => {
-          pre.removeAttribute('hidden');
-        });
-        switchButton.removeAttribute('hidden');
-        hideButton.innerHTML = hideIcon;
-        hideButton.setAttribute('title', hideTitle);
-      }
+    let buttons = expandSection.childNodes;
+    let srcContainers = parent.querySelectorAll('.org-src-container');
+    buttons[0].classList.add(selectedClass);
+    buttons.forEach((button, index) => {
+      button.addEventListener('click', (event) => {
+        event.currentTarget.classList.add(selectedClass);
+        buttons[1 - index].classList.remove(selectedClass);
+        srcContainers[1 - index].hidden = true;
+        srcContainers[index].hidden = false;
+      });
     });
+
+    parent.prepend(expandSection);
   });
 }
 
@@ -125,6 +58,7 @@ function addSidenotes() {
 }
 
 document.addEventListener("DOMContentLoaded", function() {
-  addButtons();
+  hideExpandedCode();
+  addExpandButton();
   addSidenotes();
 });
