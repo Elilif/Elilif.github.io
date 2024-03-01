c0071ecc7773878f59b9e9a014914d334096707f
eli <qq1358722950@gmail.com>
2024-03-01 23:44:26 +0800

    Rebuilding site 2024-03-01 23:44:26

--- scripts/script.js
+++ scripts/script.js
@@ -1,8 +1,13 @@
 const copyLabel = "<i class='bx bx-copy-alt' ></i>";
 
 async function copyCode(block, button) {
-  let code = block.querySelector('pre.src');
-  let text = code.innerText;
+  let codes = block.querySelectorAll('pre.src code code');
+  let text = '';
+  codes.forEach((code) => {
+    text += code.innerText;
+    text += '\n';
+  });
+  text = text.slice(0, -1);
   await navigator.clipboard.writeText(text);
   button.innerText = 'Copied';
   setTimeout(() => {
@@ -28,48 +33,52 @@ function hideExpandedCode() {
   let parents = document.querySelectorAll('.multilang');
   parents.forEach((parent) => {
     let srcBlocks = parent.querySelectorAll('.org-src-container');
-    let orig = srcBlocks[0];
-    let expanded = srcBlocks[1];
-    let label = orig.querySelector('.org-src-name');
+    if (srcBlocks.length > 1) {
+      let orig = srcBlocks[0];
+      let expanded = srcBlocks[1];
+      let label = orig.querySelector('.org-src-name');
 
-    if (label) {
-      expanded.prepend(label.cloneNode(true));
+      if (label) {
+        expanded.prepend(label.cloneNode(true));
+      }
+
+      expanded.hidden = true;
     }
-
-    expanded.hidden = true;
   });
 }
 
 function addExpandButton() {
   let parents = document.querySelectorAll('.multilang');
   parents.forEach((parent) => {
-    let expandSection = document.createElement('div');
-    const selectedClass = 'selected';
-
-    expandSection.classList.add('src-expand-section');
-    ['noweb', 'expand'].forEach((elt) => {
-      let button = document.createElement('span');
-      button.innerText = elt;
-      button.classList.add('src-' + elt);
-      expandSection.append(button);
-    });
-
-    let buttons = expandSection.childNodes;
     let srcContainers = parent.querySelectorAll('.org-src-container');
-    buttons[0].classList.add(selectedClass);
-    let id = srcContainers[0].querySelector('pre').id;
-    buttons.forEach((button, index) => {
-      button.addEventListener('click', (event) => {
-        event.currentTarget.classList.add(selectedClass);
-        buttons[1 - index].classList.remove(selectedClass);
-        srcContainers[1 - index].hidden = true;
-        srcContainers[index].hidden = false;
-        srcContainers[1 - index].querySelector('pre').removeAttribute('id');
-        srcContainers[index].querySelector('pre').id = id;
-      });
-    });
+    if (srcContainers.length > 1) {
+      let expandSection = document.createElement('div');
+      const selectedClass = 'selected';
 
-    parent.prepend(expandSection);
+      expandSection.classList.add('src-expand-section');
+      ['noweb', 'expand'].forEach((elt) => {
+        let button = document.createElement('span');
+        button.innerText = elt;
+        button.classList.add('src-' + elt);
+        expandSection.append(button);
+      });
+
+      let buttons = expandSection.childNodes;
+      buttons[0].classList.add(selectedClass);
+      let id = srcContainers[0].querySelector('pre').id;
+      buttons.forEach((button, index) => {
+        button.addEventListener('click', (event) => {
+          event.currentTarget.classList.add(selectedClass);
+          buttons[1 - index].classList.remove(selectedClass);
+          srcContainers[1 - index].hidden = true;
+          srcContainers[index].hidden = false;
+          srcContainers[1 - index].querySelector('pre').removeAttribute('id');
+          srcContainers[index].querySelector('pre').id = id;
+        });
+      });
+
+      parent.prepend(expandSection);
+    }
   });
 }
 
